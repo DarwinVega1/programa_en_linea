4e9073d228cfd6d797c8b5702472f2eb4125502c
Darwin Vega <anthony_vega@hotmail.es>
2026-05-25 19:39:53 -0600

    Cambiar a SQLite

--- config/settings.py
+++ config/settings.py
@@ -80,15 +80,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Yess_Boutique',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
