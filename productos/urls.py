544f45f9aacce30afe102c0c73a0104a4bebe520
Darwin Vega <anthony_vega@hotmail.es>
2026-05-26 22:10:15 -0600

    mensaje

--- productos/urls.py
+++ productos/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import lista_productos, nuevo_producto
 
 urlpatterns = [
-    path('', lista_productos),
-    path('nuevo/', nuevo_producto),
     path('', lista_productos, name='lista_productos'),
+    path('nuevo/', nuevo_producto, name='nuevo_producto'),
 ]
